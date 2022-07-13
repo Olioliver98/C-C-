86953f93ade98d65562b68515238a5bfed57f6cd
Oliver Peña <oliverdpz98@gmail.com>
2022-07-12 20:59:20 -0600

    Listo

---
+++ Ejercicio 11 Constructores/Alumno.cs
@@ -0,0 +1,22 @@
+// Esto es una Plantilla 
+
+public class Alumno
+{
+
+    //Estas son propiedades.
+    public int Id { get; set; }
+
+    public string PrimerNombre { get; set; }
+    public string SegundoNombre { get; set; }
+
+    //Constructor: Este debe llevar el mismo nombre de la propiedad.
+    public Alumno()
+    {
+        Id = 1;
+    }
+
+    public Alumno(int id)
+    {
+        Id = id;
+    }
+}
---
+++ Ejercicio 11 Constructores/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Propiedades
+{
+    class program
+    {
+
+        static void Main(string[] arg)
+        {
+            //Instancia
+            Alumno a = new Alumno();
+            a.Id = 1;
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Perez";
+
+            Alumno b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Maria";
+            b.SegundoNombre = "Martinez";
+
+            Alumno c = new Alumno(3);
+            c.PrimerNombre = "Pedro";
+            
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+
+        }
+
+    }
+
+
+
+}
