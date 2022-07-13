d2b8abca03e8193c7a8b82a16f35d1ecf5e4bba9
Oliver Peña <oliverdpz98@gmail.com>
2022-07-12 20:41:58 -0600

    Listo

---
+++ 1. Propiedades/Alumno.cs
@@ -0,0 +1,12 @@
+// Esto es una Plantilla 
+
+public class Alumno
+{
+
+    //Estas son propiedades.
+    public int Id { get; set; }
+
+    public string PrimerNombre { get; set; }
+
+    public string SegundoNombre { get; set; }
+}
---
+++ 1. Propiedades/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Propiedades
+{
+    class program
+    {
+
+        static void Main(string[] arg)
+        {
+            //Instancia
+            Alumno a = new Alumno();
+            a.Id = 1;
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Perez";
+
+            Console.WriteLine("Hello, World!");
+
+        }
+
+    }
+
+
+
+}
