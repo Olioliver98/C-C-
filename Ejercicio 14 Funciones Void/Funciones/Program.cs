90d04db928c57d371f9ea839bc6351375f760b75
Oliver Peña <oliverdpz98@gmail.com>
2022-07-12 21:41:37 -0600

    Listo

---
+++ Ejercicio 14 Funciones Void/Funciones/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace Funciocnes
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            Alumno a = new Alumno(1, "Juan", "Perez");
+            Alumno b = new Alumno(2, "Maria", "Martinez");
+
+            a.inactivarAlumno();
+
+            Console.WriteLine(a.nombrecompleto());
+            Console.WriteLine(b.nombrecompleto());
+
+            a.activarAlumno();
+
+            Console.WriteLine("");
+
+            Console.WriteLine(a.nombrecompleto());
+            Console.WriteLine(b.nombrecompleto());
+        }
+
+    }
+
+
+
+}
