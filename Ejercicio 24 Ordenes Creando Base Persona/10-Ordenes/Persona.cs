77d78ec07eae50416f5940f49a526ad1ddf4816f
Oliver Peña <oliverdpz98@gmail.com>
2022-07-26 17:58:41 -0600

    Finalizado

--- Ejercicio 24 Ordenes Creando Base Persona/10-Ordenes/Persona.cs
+++ Ejercicio 24 Ordenes Creando Base Persona/10-Ordenes/Persona.cs
@@ -5,12 +5,12 @@ public class Personas
      public int Codigo { get; set; }
 
     public string Nombre  { get; set; }
-    
+
     public string Telefono { get; set; }
 
     public void EnviarNotificacion()
     {
-        Console.WriteLine('Correo enviado a ' + Nombre);
+        Console.WriteLine("Correo enviado a " + Nombre);
     }
 
 }
